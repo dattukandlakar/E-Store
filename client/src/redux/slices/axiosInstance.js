9841bd32acc40666d89ad6366496a1d4fde61a90
dattukandlakar <129265101+dattukandlakar@users.noreply.github.com>
2025-08-26 14:16:12 +0530

    Update axiosInstance.js

--- client/src/redux/slices/axiosInstance.js
+++ client/src/redux/slices/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Use Vite dev proxy for API calls during development
 const instance = axios.create({
-  baseURL: 'https://ec-storee.netlify.app',
+  baseURL: 'https://e-store-1-gr1l.onrender.com',
 });
 
 export default instance;
